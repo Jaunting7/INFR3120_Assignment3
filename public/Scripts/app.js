12d43f27aee5b67edc5c544d545cf14bef6db546
Jason Kong <jjkong149@hotmail.com>
2023-11-02 17:37:48 -0400

    A2 start

---
+++ public/Scripts/app.js
@@ -0,0 +1,10 @@
+// Single Page Application
+// Client Side
+
+// Immediate invoked function expression
+(function() {
+    function Start() {
+        console.log("App Started");
+    }
+    window.addEventListener("load", Start());
+})();
